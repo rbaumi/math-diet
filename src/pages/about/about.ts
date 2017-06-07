0fed523422e96268486fe0ebd47eeabf723b17de
Rafal Baumann <rafal.baumann@newtec.dk>
2017-06-07 13:02:04 +0200

    Icons and tabs

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+    selector: 'page-about',
+    templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+        this.presentLoading();
+        location.href = "https://weightctrlblog.wordpress.com";
+    }
 
-  }
-
+    presentLoading() {
+        let loader = this.loadingCtrl.create({
+            content: "Please wait...",
+            duration: 3000
+        });
+        loader.present().then(() => {
+            console.log (1);
+        });
+    }
 }
