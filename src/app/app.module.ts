ee4f7a69981646b07f333830cc1829cd0edbd070
Rafal Baumann <rafal.baumann@newtec.dk>
2017-06-07 12:21:32 +0200

    Creating initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { DietPage } from '../pages/diet/diet';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -15,7 +15,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    DietPage,
     HomePage,
     TabsPage
   ],
@@ -27,7 +27,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    DietPage,
     HomePage,
     TabsPage
   ],
--- src/pages/diet/diet.ts
+++ src/pages/diet/diet.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-diet',
+  templateUrl: 'diet.html'
 })
-export class ContactPage {
+export class DietPage {
 
   constructor(public navCtrl: NavController) {
 
