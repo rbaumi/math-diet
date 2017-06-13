3767b7e28eb99ba2966456143e562ceaaf77ce1e
Rafal Baumann <rafal.baumann@gmail.com>
2017-06-14 00:42:10 +0200

    Adjusting home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { DietPage } from '../diet/diet';
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
 
-  }
-
+    }
+    moveToDiets(): void {
+        this.navCtrl.setRoot(DietPage);
+    }
 }
